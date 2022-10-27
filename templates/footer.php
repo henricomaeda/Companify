e62fb31204d4f57a9f3bd741eb95cafd925bc0b4
Henrico Pelozato Maeda <henrico.maeda@gmail.com>
2022-10-27 01:55:17 -0300

    Enviando arquivos

---
+++ templates/footer.php
@@ -0,0 +1,28 @@
+        <footer>
+			<nav>
+				<div class="social-media">
+					<nav class="social-media">
+						<a href="https://facebook.com">
+							<img src="<?= $BASE_URL ?>./assets/social_networks/Facebook.png" />
+							<span> Facebook </span>
+						</a>
+						<a href="https://instagram.com">
+							<img src="<?= $BASE_URL ?>./assets/social_networks/Instagram.png" />
+							<span> Instagram </span>
+						</a>
+						<a href="https://twitter.com">
+							<img src="<?= $BASE_URL ?>./assets/social_networks/Twitter.png" />
+							<span> Twitter </span>
+						</a>
+						<a href="https://whatsapp.com">
+							<img src="<?= $BASE_URL ?>./assets/social_networks/WhatsApp.png" />
+							<span> WhatsApp </span>
+						</a>
+					</nav>
+				</div>
+				<a href="https://www.flaticon.com/authors/freepik"><span class="underline"> All default icons created by Freepik - Flaticon </span></a>
+				<span> Copyright © 2022 Shopiest developed by Henrico Maeda </span>
+			</nav>
+        </footer>
+    </body>
+</html>
