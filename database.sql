ba29ad24fb6e5449d27e4cbaf0f35e19797f6a31
Henrico Pelozato Maeda <henrico.maeda@gmail.com>
2022-11-27 21:36:40 -0300

    Atualizando arquivos

---
+++ database.sql
@@ -0,0 +1,47 @@
+create database if not exists commercify;
+use commercify;
+
+drop table if exists `commercify`.`products`;
+drop table if exists `commercify`.`trades`;
+drop table if exists `commercify`.`users`;
+
+create table `commercify`.`users` (
+	`use_email` varchar(200) not null,
+	`use_name` varchar(200) not null,
+	`use_password` varchar(200) not null,
+	`use_description` varchar(200) null,
+	`use_picture` varchar(200) null,
+	PRIMARY KEY (`use_email`)
+) ENGINE = InnoDB;
+
+create table `commercify`.`trades` (
+	`tra_id` int not null AUTO_INCREMENT,
+	`tra_name` varchar(200) not null,
+	`tra_description` varchar(200) null,
+	`tra_picture` varchar(200) null,
+	`use_email` varchar(200) not null,
+	PRIMARY KEY (`tra_id`),
+	INDEX `fk_trades_users_idx` (`use_email` ASC),
+	CONSTRAint `fk_trades_users`
+		FOREIGN KEY (`use_email`)
+		REFERENCES `commercify`.`users` (`use_email`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+) ENGINE = InnoDB;
+
+create table `commercify`.`products` (
+	`pro_id` int not null AUTO_INCREMENT,
+	`pro_name` varchar(200) not null,
+	`pro_price` DECIMAL not null,
+	`pro_discount` int not null,
+	`pro_description` varchar(200) null,
+	`pro_picture` varchar(200) null,
+	`tra_id` int not null,
+	PRIMARY KEY (`pro_id`),
+	INDEX `fk_products_trades1_idx` (`tra_id` ASC),
+	CONSTRAint `fk_products_trades1`
+		FOREIGN KEY (`tra_id`)
+		REFERENCES `commercify`.`trades` (`tra_id`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+) ENGINE = InnoDB;
--- pt_BR/connection.php
+++ pt_BR/connection.php
@@ -3,15 +3,43 @@
 	// $message -> setMessage(true, "return", "Seja muito bem-vindo, usuário!");
 ?>
 <div class="connection">
-	<form>
+	<form method="POST" enctype="application/json" action="<?= $BASE_URL ?>/../my_account.php">
 		<span class="title"> Conectar </span>
-		<input type="text" id="search" name="search" placeholder="Procurar comércios e produtos..." />
-		<button type="submit"></button>
+		<hr />
+		<div>
+			<input type="email" name="email" required />
+			<label> E-mail </label>
+		</div>
+		<div>
+			<input type="password" name="password" required />
+			<label> Senha </label>
+		</div>
+		<input type="submit" id="submit" value="Conectar" />
+		<hr />
+		<a href="<?= $BASE_URL ?>/../forgot.php">
+			<span> Esqueceu a senha? </span>
+		</a>
 	</form>
 	<form>
 		<span class="title"> Cadastrar </span>
-		<input type="text" id="search" name="search" placeholder="Procurar comércios e produtos..." />
-		<button type="submit"></button>
+		<hr />
+		<div>
+			<input type="text" name="name" required />
+			<label> Nome completo </label>
+		</div>
+		<div>
+			<input type="email" name="email" required />
+			<label> E-mail </label>
+		</div>
+		<div>
+			<input type="password" name="password" required />
+			<label> Senha </label>
+		</div>
+		<div>
+			<input type="password" name="confirm_password" required />
+			<label> Confirmação da senha </label>
+		</div>
+		<input type="submit" id="submit" value="Cadastrar" />
 	</form>
 </div>
 <?php require_once("./templates/footer.php") ?>
